cb8f0a8057d0543d70cd5b34323e29cf3b93d866
Evan Rutten <etrutten@gmail.com>
2022-12-18 00:47:23 -0500

    tank movement + fixed labels

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
 
-const SPEED = 300
+const MOVEMENT_SPEED = 300.0
+const ROTATION_SPEED = 2.0
+
+const LABEL_OFFSET_X = 50.0
+const LABEL_OFFSET_Y = 70.0
 
 func is_local_authority():
 	return $Networking/MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id()
@@ -8,24 +12,35 @@ func is_local_authority():
 func _ready():
 	$Networking/MultiplayerSynchronizer.set_multiplayer_authority(str(name).to_int())
 	$Camera2D.current = is_local_authority()
-	$UI/Label.text = name
+	$Label.set_as_top_level(true)
+	$Label.text = name
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	if not is_local_authority():
 		if not $Networking.processed_position:
 			position = $Networking.sync_position
 			$Networking.processed_position = true
 		velocity = $Networking.sync_velocity
+		rotation = $Networking.sync_rotation
 		move_and_slide()
+		update_label()
 		return
 	
-	var direction = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down"))
-	if direction:
-		velocity = direction.normalized() * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("left"):
+		rotation -= ROTATION_SPEED * delta
+	if Input.is_action_pressed("right"):
+		rotation += ROTATION_SPEED * delta
+	if Input.is_action_pressed("up"):
+		velocity = Vector2(0, -MOVEMENT_SPEED).rotated(rotation)
+	if Input.is_action_pressed("down"):
+		velocity = Vector2(0, +MOVEMENT_SPEED).rotated(rotation)
 	move_and_slide()
+	update_label()
 	
 	$Networking.sync_position = position
 	$Networking.sync_velocity = velocity
+	$Networking.sync_rotation = rotation
+
+func update_label() -> void:
+	$Label.set_global_position(Vector2(position.x - LABEL_OFFSET_X, position.y - LABEL_OFFSET_Y))
